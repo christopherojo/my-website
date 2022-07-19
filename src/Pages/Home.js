d08f6bb1e71f9df31829b7fe0009c58cd84c9764
ChristopherOjo <cojo@uwo.ca>
2022-07-19 13:00:23 -0400

    Website Basics

---
+++ src/Pages/Home.js
@@ -0,0 +1,21 @@
+import './Home.css'
+
+const NavBar = () => {
+  return (
+    <>
+      <table>
+        <col> AYO</col>
+      </table>
+    </>
+  );
+}
+
+const Home = () => {
+  return (
+    <>
+      <NavBar />
+    </>
+  );
+};
+
+export default Home;

09fe44f4b2d5eff56c22881cddf9b3849a02e826
ChristopherOjo <cojo@uwo.ca>
2022-07-25 23:56:36 -0400

    Introduction

--- src/Navigation/Navbar.js
+++ src/Navigation/Navbar.js
@@ -31,7 +31,7 @@ const Navbar = () => {
             </a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#">
+            <a className="nav-link" href="#connect">
               Pricing
             </a>
           </li>
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,11 +1,24 @@
 import './Home.css'
+import LinkedIn from './Images/LinkedIn.jpg'
 
 const Home = () => {
   return (
     <div className='main'>
       <h1>Main Picture</h1>
       <div className='working'>
-        <p>Tandom words</p>
+        <table>
+          <td style={{width: "10%", paddingRight: "30px"}}>
+            <div>
+              <img className="profile" src={LinkedIn} alt="Profile Picture"/>
+            </div>
+          </td>
+          <td style={{textAlign: 'left', verticalAlign: 'middle'}}>
+            <div className='introText'>
+              <h2>Hi! I am Christopher!</h2>
+              <p>I am a second year software engineering student at Western University!</p>
+            </div>
+          </td>
+        </table>
       </div>
       <h1>About Me</h1>
       <div className='working'>
@@ -27,8 +40,9 @@ const Home = () => {
       <div className='working'>
         <p>Tandom words</p>
       </div>
-      <h1 className='connect'>Connect</h1>
-      <div className='working'>
+      <a id="connect"></a>
+      <h1 className="connectHeader">Connect</h1>
+      <div className='connect'>
         <p>Tandom words</p>
       </div>
     </div>

ccfe3451f1bebd685b5c543c4957eb2efa08b96f
ChristopherOjo <cojo@uwo.ca>
2022-08-03 00:21:36 -0400

    messing around background photos

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,20 +1,24 @@
 import './Home.css'
 import LinkedIn from './Images/LinkedIn.jpg'
 import Sunset from './Images/Sunset.png'
+import Windows from './Images/Linked Windows.jpg'
 
 const Home = () => {
   return (
     <div className='main'>
-      <h1>Main Picture</h1>
-      <img className="background-photo" src={Sunset} alt="Sunset Photo" />
       <div className='introduction'>
+        <h1>Main Picture</h1>
+        <img src={Windows} alt="Introduction Picture"/>
+      </div>
+      <img className="background-photo" src={Sunset} alt="Sunset Photo" />
+      <div className='summary'>
         <table>
-          <td style={{width: "30%", paddingRight: "30px"}}>
+          <td style={{width: "30%", paddingRight: "3%"}}>
             <div>
               <img className="profile" src={LinkedIn} alt="Profile Picture"/>
             </div>
           </td>
-          <td style={{textAlign: 'left', verticalAlign: 'middle'}}>
+          <td style={{textAlign: 'center', verticalAlign: 'middle'}}>
             <div className='introText'>
               <h2>Hi! I am Christopher!</h2>
               <p>I am a second year software engineering student at Western University!</p>

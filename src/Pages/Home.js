23dd34dbbfb83c890e61a2fa028a69485768543a
ChristopherOjo <cojo@uwo.ca>
2022-08-03 13:19:18 -0400

    Issac broken

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -6,26 +6,28 @@ import Windows from './Images/Linked Windows.jpg'
 const Home = () => {
   return (
     <div className='main'>
-      <div className='introduction'>
-        <h1>Main Picture</h1>
-        <img src={Windows} alt="Introduction Picture"/>
-      </div>
-      <img className="background-photo" src={Sunset} alt="Sunset Photo" />
-      <div className='summary'>
-        <table>
-          <td style={{width: "30%", paddingRight: "3%"}}>
-            <div>
-              <img className="profile" src={LinkedIn} alt="Profile Picture"/>
-            </div>
-          </td>
-          <td style={{textAlign: 'center', verticalAlign: 'middle'}}>
-            <div className='introText'>
-              <h2>Hi! I am Christopher!</h2>
-              <p>I am a second year software engineering student at Western University!</p>
-            </div>
-          </td>
-        </table>
-      </div>
+      <section className='section-welcome'>
+        <div className='introduction'>
+          <h1>Hello! :D</h1>
+          <h5>I'm Christopher Ojo, an Ivey and Software Engineering Dual Degree student at the University of Western Ontario as well as an aspiring software developer and data scientist.</h5>
+        </div>
+        <img className="background-photo" src={Sunset} alt="Sunset Photo" />
+        <div className='summary'>
+          <table>
+            <td style={{width: "30%", paddingRight: "3%"}}>
+              <div>
+                <img className="profile" src={LinkedIn} alt="Profile Picture"/>
+              </div>
+            </td>
+            <td style={{textAlign: 'center', verticalAlign: 'middle'}}>
+              <div className='introText'>
+                <h2>Hi! I am Christopher!</h2>
+                <p>I am a second year software engineering student at Western University!</p>
+              </div>
+            </td>
+          </table>
+        </div>
+      </section>
       <h1>About Me</h1>
       <div className='working'>
         <p>Tandom words</p>

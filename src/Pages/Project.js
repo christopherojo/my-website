f18be3d6a169c7d03549a2396c8db596e1ce40d2
ChristopherOjo <cojo@uwo.ca>
2022-08-15 22:28:53 -0400

    project page template

--- src/Pages/Project.js
+++ src/Pages/Project.js
@@ -2,18 +2,51 @@ import './Project.css'
 
 const Header = () => {
   return (
-    <div>
-
+    <div className='project-header'>
+      <div className='project-header-title'>
+        <h1>Project Name</h1>
+        <h3>Project Link</h3>
+      </div>
     </div>
   );
 }
 
 const Information = () => {
   return (
-    <div>
-      <div className='project-summary'>
-        <h1>About</h1>
-        <p>Super Genius</p>
+    <div className='project-info'>
+      <div className='project-description'>
+        <div className='project-summary'>
+          <h1>About</h1>
+          <p>Super Genius</p>
+        </div>
+        <div className='project-inspiration'>
+          <h1>Inspiration</h1>
+          <p>Super Genius</p>
+        </div>
+        <div className='project-what'>
+          <h1>What it does</h1>
+          <p>Super Genius</p>
+        </div>
+        <div className='project-how'>
+          <h1>How we built it</h1>
+          <p>Super Genius</p>
+        </div>
+        <div className='project-challenges'>
+          <h1>Challenges we ran into</h1>
+          <p>Super Genius</p>
+        </div>
+        <div className='project-accomplishments'>
+          <h1>Accomplishments</h1>
+          <p>Super Genius</p>
+        </div>
+        <div className='project-learn'>
+          <h1>What we learned</h1>
+          <p>Super Genius</p>
+        </div>
+        <div className='project-next'>
+          <h1>What's next</h1>
+          <p>Taken from Devpost</p>
+        </div>
       </div>
     </div>
   );
